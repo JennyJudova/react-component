301578b03d43843167e01321c2a1af902c704124
JennyJudova <jenny@Jennys-MacBook-Pro.local>
2019-12-13 18:15:19 +0000

    flexbox added, data showing on page

--- src/components/FileDownload.js
+++ src/components/FileDownload.js
@@ -2,15 +2,42 @@
 import React, { useEffect } from 'react';
 
 export default function YourLocation() {
-  const Data = require('../../file.json');
+  const data = require('../../file.json');
 
   useEffect(() => {
-    console.log(Data);
+    console.log(data);
   }, []);
 
   return (
-    <div>
-      <h2>component connected</h2>
+    <div className="fileWrapper">
+      <div className="downloadWrapper">
+        <input type="checkbox" />
+        <p>Selected</p>
+        <p>Download Selected</p>
+      </div>
+      <div className="download">
+        <div className="downloadHeadings">
+          <p>Name</p>
+          <p>Device</p>
+          <p>Path</p>
+          <p>Status</p>
+        </div>
+        {data && (
+          <ul>
+            {data.map((file) => {
+              return (
+                <li key={file.name}>
+                  <input type="checkbox" />
+                  <p>{file.name}</p>
+                  <p>{file.device}</p>
+                  <p>{file.path}</p>
+                  <p>{file.status}</p>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
